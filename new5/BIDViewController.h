cb4fcef9c2ecc90e5a55791f576e78aea2816aa8
dongjieArya <rashmi@aryamac1.local>
2014-08-11 17:27:25 -0700

    add a new BIDViewController

---
+++ new5/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  new5
+//
+//  Created by rashmi on 8/11/14.
+//  Copyright (c) 2014 arya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
